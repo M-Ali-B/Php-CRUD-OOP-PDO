2170a41ac1fb37ecb97fac6fc23fe52868dacd1b
Ali <alibabatorjan001@gmail.com>
2021-10-07 00:33:27 +0500

    Added Port Number Variable
     please use a port number(MYSQL port Number) for avoiding refusal of target machine

--- config/db.php
+++ config/db.php
@@ -7,9 +7,9 @@
         $username = "root";
         $password = "";
         $dbname = "studentdatabase";
-
+        $port = "XXXX"; // please use port number(MYSQL port Number) for avoiding refusal of target machine 
         try {
-            $conn = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
+            $conn = new PDO("mysql:host=$servername;port=$port dbname=$dbname", $username, $password);
             // set the PDO error mode to exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             //echo "Connected successfully"; 
